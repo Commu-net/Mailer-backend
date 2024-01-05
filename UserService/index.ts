b6d48aaf30c5d5ce6327bc8e9fe3eee1080530f7
Ayush Vishwwakarma <ayushvish6555@gmail.com>
2024-01-05 17:05:58 +0530

    Init

---
+++ UserService/index.ts
@@ -0,0 +1,10 @@
+import express  , {Express} from 'express';
+const app  : Express= express();
+const port = 3000;
+app.get('/', (req , res ) =>  {
+    res.send('Hello World!');
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
+
