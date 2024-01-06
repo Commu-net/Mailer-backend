5daf83ffd9131515c522fdc8dd104f8877286f92
chahat sagar <chahatsagar2003@gmail.com>
2024-01-06 16:27:47 +0530

    .

--- EmailService/index.ts
+++ EmailService/index.ts
@@ -1,6 +1,8 @@
 import express ,{ Express , Request , Response} from "express";
 import dotenv from "dotenv";
 
+dotenv.config()
+
 const app : Express = express();
 const PORT = process.env.PORT || 8080;
 
