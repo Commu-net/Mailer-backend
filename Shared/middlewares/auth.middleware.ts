12af36b27882a5f26acded6a2dd6b0563879b9c5
chahat sagar <chahatsagar2003@gmail.com>
2024-01-08 09:17:57 +0530

    auth middleware

--- Shared/middlewares/auth.middleware.ts
+++ Shared/middlewares/auth.middleware.ts
@@ -1,14 +1,17 @@
-const  isLoggedIn =  (req : Request, res : Response, next : any ) =>   {
+import { Response } from 'express';
+
+interface AuthenticatedRequest extends Request{
+    user? : any;
+}
+
+const  isLoggedIn =  (req : AuthenticatedRequest, res : Response, next : any ) =>   {
     
-    
-    
-    if(req?.user)  {
-        next( );
+    if(req.user)  {
+        next();
         
     }
     else  { 
-        res.status(401)
-        .send("You are not authenticated");
+        res.status(401).send();
     }
 }
 
