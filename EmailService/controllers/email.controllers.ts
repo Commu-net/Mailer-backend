2e89c935ac2f63fe8acc80a4a704f47b8358e216
chahat sagar <109112505+chahatsagarmain@users.noreply.github.com>
2024-01-21 16:17:08 +0530

    Sending emails with attachments

--- EmailService/controllers/email.controllers.ts
+++ EmailService/controllers/email.controllers.ts
@@ -2,8 +2,16 @@ import { NextFunction, Request, Response } from "express";
 import User, { userInterface } from "../models/user.model";
 import { google } from "googleapis";
 import MailComposer from "nodemailer/lib/mail-composer";
+import formidable from 'formidable';
+import { readFileSync, unlink, writeFile } from "fs";
+import { Buffer } from "buffer";
+import { promisify } from "util";
 
-export async function sendMail(req: Request, res: Response, next: NextFunction) {
+interface AuthorizedRequest extends Request{
+    user? : any
+}
+
+export async function sendMail(req: AuthorizedRequest, res: Response, next: NextFunction) {
     try {
         // Temporarily checking sending mail
         const user: userInterface | null = await User.findOne({ "email": "chahatsagar2003@gmail.com" });
@@ -24,8 +32,8 @@ export async function sendMail(req: Request, res: Response, next: NextFunction) 
 
         const mailOptions = {
             to: 'chahatsagar22003@gmail.com',
-            cc: 'cc1@example.com, cc2@example.com',
-            replyTo: 'amit@labnol.org',
+            // cc: 'itsyash9211@gmail.com, cc2@example.com',
+            // replyTo: 'amit@labnol.org',
             subject: 'Hello world',
             text: 'This email is sent from the command line',
         };
@@ -47,16 +55,99 @@ export async function sendMail(req: Request, res: Response, next: NextFunction) 
     }
 }
 
+const writeFileAsync = promisify(writeFile);
+const unlinkAsync = promisify(unlink);
 
+export async function sendMass(req: AuthorizedRequest, res: Response, next: NextFunction) {
+    const form: any = formidable({
+        minFileSize: 1,
+        maxFiles: 5,
+        maxFileSize: 10 * 1024 * 1024,
+    });
 
+    form.parse(req, async (err: any, fields: any, files: any) => {
+        if (err) {
+            next(err);
+            return;
+        }
 
-// const accessToken = await new Promise((resolve, reject) => {
-//     oauth2Client.getAccessToken((err, token) => {
-//       if (err) {
-//         console.log("*ERR: ", err)
-//         reject();
-//       }
-//       resolve(token); 
-//     });
-//   });
-// console.log(accessToken);
+        const fileNames: string[] = [];
+
+        try {
+
+            await Promise.all(Object.keys(files).map(async (key : any) => {
+                const filename: string = files[key][0].originalFilename;
+                const rawFile: Buffer = readFileSync(files[key][0].filepath);
+
+                await writeFileAsync(`./temp/${filename}`, rawFile);
+                fileNames.push(filename);
+            }));
+
+            const emails: string[] = fields.emails[0].split(",");
+            const sender: string = fields.sender[0];
+            const subject: string = fields.subject[0];
+            const text: string = fields.text[0];
+
+            await Promise.all(emails.map(async (email: string) => {
+                await sendOneMail(email, sender, fileNames, subject, text);
+            }));
+
+            await Promise.all(fileNames.map(async (element: string) => {
+                await unlinkAsync(`./temp/${element}`);
+            }));
+
+            return res.status(200).json({ "message": "Successful" });
+        } catch (error) {
+            console.log(error);
+            next(error);
+        }
+    });
+}
+
+async function sendOneMail(mail : string , senderMail : string ,fileNames : string[] , subject : string , text : string){
+    try {
+        // Temporarily checking sending mail
+        const user: userInterface | null = await User.findOne({ "email": senderMail });
+        if (!user) return new Error("User not found");
+
+        const oauth2Client = new google.auth.OAuth2(
+            process.env.GOOGLE_CLIENT_ID,
+            process.env.GOOGLE_CLIENT_SECRET,
+            // process.env.CLIENT_URL
+        );
+        
+
+        oauth2Client.setCredentials({
+            access_token: user.acessToken,
+            refresh_token: user.rToken,
+        });
+        
+        const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
+
+        const mailOptions = {
+            to: mail,
+            subject: subject,
+            text: text,
+            attachments: fileNames.length > 0 ? [
+                {
+                    filename: fileNames[0],
+                    path: `./temp/${fileNames[0]}`,
+                }
+            ] : [],
+        };
+
+        const mailComposer = new MailComposer(mailOptions);
+        const message = await mailComposer.compile().build();
+
+        await gmail.users.messages.send({
+            userId: 'me',
+            requestBody: {
+                raw: message.toString('base64')
+            }
+        });
+        console.log(`email send by ${mail}`);
+
+    } catch (error: any) {
+        console.log(error);
+    }
+}
--- EmailService/routes/emailRoutes.ts
+++ EmailService/routes/emailRoutes.ts
@@ -1,9 +1,7 @@
 import { Router } from "express";
-import formidable from 'formidable';
 import { Request, Response, NextFunction } from "express";
-import { readFileSync, unlink, writeFile } from "fs";
-import { Buffer } from "buffer";
-import { sendMail } from "../controllers/email.controllers";
+
+import { sendMail , sendMass} from "../controllers/email.controllers";
 
 const router: Router = Router();
 
@@ -11,56 +9,9 @@ router.get("/mail", (req: Request, res: Response, next: NextFunction) => {
     return res.status(200).json("post on this route");
 });
 
-router.post("/mail", (req: Request, res: Response, next: NextFunction) => {
+router.post("/mail", sendMass);
 
-    const form: any = formidable({
-        minFileSize: 1,
-        maxFiles: 5,
-        maxFileSize: 10 * 1024 * 1024,
-        // uploadDir: "./temp",
-        // filename(name,ext) {
-        //     console.log(path.extname(name));
-        //     return name + ext;
-        // }
-    });
-
-    form.parse(req, (err: any, fields: any, files: any) => {
-        if (err) {
-            next(err);
-            return;
-        }
-        console.log(fields);
-
-        const fileNames: string[] = []
-
-        for (const key in files) {
-
-            const filename : string= files[key][0].originalFilename;
-            const rawFile : Buffer = readFileSync(files[key][0].filepath);
-
-            writeFile(`./temp/${filename}`, rawFile , (err) => {
-                next(err);
-                return;
-            });
-            fileNames.push(filename);
-        }
-
-        //Send mail logic here 
-        
-        fileNames.forEach(async (element: string) => {
-
-            await unlink(`./temp/${element}`, (err) => {
-                next(err);
-                return;
-            });
-
-        });
-        return res.status(200).json({ "message": "all is well" });
-
-    });
-});
-
-router.post("/send",sendMail);
+router.post("/send",sendMail); //send mails without attachments
 
 export default router;
 
